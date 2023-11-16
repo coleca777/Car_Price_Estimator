81deb13254ef1cd474d8c4ad256669e112b0afbb
coleca777 <150979469+coleca777@users.noreply.github.com>
2023-11-15 21:37:21 -0500

    Add files via upload

---
+++ vehicle_package_grouping.py
@@ -0,0 +1,302 @@
+category_lst = [
+  [
+     "Bose High End Sound Package",
+     "Premium Sound Package",
+     "Sound Package",
+     "Premium Audio Package",
+     "Audio Package"
+  ],
+  [
+     "Acoustical Package",
+     "Luxury Sound Insulation Package",
+     "Quiet Package"
+  ],
+  [
+     "Storage Package",
+     "Cargo Package"
+  ],
+  [
+     "Chrome Wheels",
+     "Dual Rear Wheels",
+     "Alloy Wheels",
+     "Premium Wheels",
+     "5th Wheel",
+     "Steel Wheels"
+  ],
+  [
+     "CarPlay",
+     "DVD Entertainment System",
+     "Bluetooth",
+     "Navigation System",
+     "Technology Package",
+     "Multimedia Package",
+     "Android Auto"
+  ],
+  [
+     "Skid Plate Package"
+  ],
+  [
+     "Remote Vehicle Starter Prep Package",
+     "Remote Start"
+  ],
+  [
+     "Solid Smooth Ride Suspension Package",
+     "Standard Suspension Package",
+     "Adaptive Suspension",
+     "Luxury Ride Suspension Package",
+     "Premium Smooth Ride Suspension Package",
+     "Autoride Suspension Package",
+     "Suspension Package",
+     "Adaptive Ride Package",
+     "Performance Handling Package",
+     "All-Wheel Steering",
+     "Off Road Package"
+  ],
+  [
+    "Parking Sensors",
+  ],
+  [
+    "Backup Camera",
+  ],
+  [
+    "Blind Spot Monitoring",
+  ],
+  [
+    "Adaptive Cruise Control",
+  ],
+  [
+     "Driver Assistance Package",
+     "Driver Confidence Package"
+  ],
+  [
+     "Bi Xenon Headlamp Package"
+  ],
+  [
+     "Safety Package",
+     "Security Package"
+  ],
+  [
+     "LE Package",
+     "XLE Package",
+     "Z 71 Package",
+     "Elite Package",
+     "Journey Package",
+     "Z71 Package"
+  ],
+  [
+     "LS Package",
+     "Comfort Package",
+     "SXT Package",
+     "2lt Package"
+  ],
+  [
+     "Lariat Package",
+     "XLT Package",
+     "LT Package",
+     "Executive Package",
+     "Grand Tour Package",
+     "Z85 Package"
+  ],
+  [
+     "AMG Sport Package",
+     "M Sport Package",
+     "RS Package"
+  ],
+  [
+     "King Ranch Package",
+     "Heavy Duty Trailer Package"
+  ],
+  [
+     "SE Package"
+  ],
+  [
+     "SL Package"
+  ],
+  [
+     "SLE Package"
+  ],
+  [
+     "SLT Package"
+  ],
+  [
+     "SR5 Grade Package",
+     "SR5 Package"
+  ],
+  [
+     "TRD Package"
+  ],
+  [
+     "Preferred Accessory Package",
+     "Preferred Equipment Package",
+     "Preferred Package",
+     "Preferred Premium Accessory Package"
+  ],
+  [
+     "Luxury Package",
+     "Premium Package",
+     "Ultimate Package"
+  ],
+  [
+     "P01 Premium Package",
+     "P1 Package",
+     "Premium 1 Package"
+  ],
+  [
+     "P2 Package",
+     "Premium 2 Package"
+  ],
+  [
+     "Premium Plus Package"
+  ],
+  [
+     "Sport Chrono Package",
+     "Sport Activity Package",
+     "S Line Sport Package",
+     "Sport Package",
+     "S Line Package"
+  ],
+  [
+     "Convenience Light Package",
+     "Light Package",
+     "Courtesy Lights Illum Package"
+  ],
+  [
+     "Convenience Package"
+  ],
+  [
+     "Convenience Plus Package"
+  ],
+  [
+     "Ambient Light Package"
+  ],
+  [
+     "Appearance Package",
+     "Appearance and Protection Package"
+  ],
+  [
+     "Winter Package",
+     "Heat Package",
+     "Cold Weather Package"
+  ],
+  [
+     "Levant Package"
+  ],
+  [
+     "F01 Package"
+  ],
+  [
+     "License Plate Front Mount Package"
+  ],
+  [
+     "Wood Trim Package",
+     "Wood Upgrade Package"
+  ],
+  [
+     "101A Mid Equipment Group"
+  ],
+  [
+     "501A Mid Equipment Group"
+  ],
+  [
+     "301A Mid Equipment Group"
+  ],
+  [
+     "302A Luxury Equipment Group"
+  ],
+  [
+     "502A Luxury Equipment Group"
+  ],
+  [
+     "601A Luxury Equipment Group"
+  ],
+  [
+     "701A Luxury Equipment Group"
+  ],
+  [
+     "802A Luxury Equipment Group"
+  ],
+  [
+     "Memory Package"
+  ],
+  [
+     "Multi Zone Climate Control",
+     "Rear Climate Package"
+  ],
+  [
+     "Sunroof/Moonroof"
+  ],
+  [
+     "Graphics Package"
+  ],
+  [
+     "Value Package"
+  ],
+  [
+     "Extra Value Package"
+  ],
+  [
+     "Smoker Package"
+  ],
+  [
+     "Special Edition Package",
+     "Limited Package"
+  ],
+  [
+     "Carbon Ceramic Brakes"
+  ],
+  [
+     "Handicap Accessible"
+  ],
+  [
+     "Leather Seats"
+  ],
+  [
+     "Heated Seats"
+  ],
+  [
+     "Third Row Seating"
+  ],
+  [
+     "Ground Effects Package"
+  ],
+  [
+     "Power Mirror Package"
+  ],
+  [
+     "Power Package"
+  ],
+  [
+     "Tow Package",
+     "Trailer Package"
+  ],
+  [
+     "Tour Package"
+  ],
+  [
+     "Trim Package"
+  ],
+  [
+     "Skid Plate Package"
+  ],
+  [
+     "Upgrade Package"
+  ],
+  [
+     "Utility Package"
+  ],
+  [
+     "Customer Preferred Package"
+  ],
+  [
+     "Popular Equipment Package"
+  ],
+  [
+     "Quick Order Package"
+  ],
+  [
+     "Ultra Luxury Package"
+  ],
+  [
+     "Performance Package"
+  ]
+]
